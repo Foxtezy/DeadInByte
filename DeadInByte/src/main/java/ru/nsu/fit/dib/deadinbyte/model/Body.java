ac472c4245917cf57f3fac416811039fe0ddfd11
xDm1try <112641839+xDm1try@users.noreply.github.com>
2022-10-01 15:29:59 +0700

    add collider

--- DeadInByte/src/main/java/ru/nsu/fit/dib/deadinbyte/model/Body.java
+++ DeadInByte/src/main/java/ru/nsu/fit/dib/deadinbyte/model/Body.java
@@ -1,6 +1,7 @@
 ﻿package ru.nsu.fit.dib.deadinbyte.model;
 
 import javafx.geometry.Point2D;
+import javafx.geometry.Rectangle2D;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,4 +12,5 @@ import lombok.NoArgsConstructor;
 public class Body {
     private Point2D coordinates;
     private String image; // TODO: 01.10.2022 картинка
+    private Rectangle2D collider;
 }
