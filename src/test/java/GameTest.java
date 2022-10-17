484f8f912da5060772dd0609a34456ddfd9cae76
Kolacom <nmahov2003@gmail.com>
2022-10-17 23:27:25 +0700

    CI correct 4

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+  @Test
+  public void sampleTest() {
+    Assertions.assertEquals(2, 2);
+  }
+
+}
