dd08da30e4ce8a244a16e5868f27d8344fa505f1
xDm1try <112641839+xDm1try@users.noreply.github.com>
2023-03-22 10:48:40 +0700

    made EnemiesFactory

---
+++ src/main/java/ru/nsu/fit/dib/projectdib/entity/creatures/EnemiesFactory.java
@@ -0,0 +1,64 @@
+package ru.nsu.fit.dib.projectdib.entity.creatures;
+
+import ru.nsu.fit.dib.projectdib.RandomSystem;
+import ru.nsu.fit.dib.projectdib.entity.weapons.WeaponFactory;
+import ru.nsu.fit.dib.projectdib.entity.weapons.enums.WeaponType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnemiesFactory {
+	public static Creature getEnemy(EnemyType enemyType){
+		List<Integer> specsList = RandomSystem.generateCreatureSpecs();
+		return switch (enemyType) {
+			case Ogre -> new Creature.CreatureBuilder(CreatureRarity.special, "Ogre")
+				.setID(0)
+				.setSpecs(RandomSystem.d(10),
+					specsList.get(0), specsList.get(4), specsList.get(1),
+					specsList.get(2), specsList.get(5), specsList.get(3))
+				.setStartWeapon(WeaponFactory.getWeapon(WeaponFactory.Weapons.Hand))
+				.setSkill(WeaponType.melee, 5)
+				.build();
+			case ZOMBIE -> new Creature.CreatureBuilder(CreatureRarity.special, "Zombie")
+				.setID(1)
+				.setSpecs(RandomSystem.d(5),
+					specsList.get(2), specsList.get(3), specsList.get(1),
+					specsList.get(4), specsList.get(5), specsList.get(5))
+				.setStartWeapon(WeaponFactory.getWeapon(WeaponFactory.Weapons.Sword))
+				.setSkill(WeaponType.shooting, 2)
+				.build();
+			case SKELETON -> new Creature.CreatureBuilder(CreatureRarity.special, "Zombie")
+				.setID(2)
+				.setSpecs(RandomSystem.d(5),
+					specsList.get(2), specsList.get(3), specsList.get(1),
+					specsList.get(4), specsList.get(5), specsList.get(5))
+				.setStartWeapon(WeaponFactory.getWeapon(WeaponFactory.Weapons.Bow))
+				.setSkill(WeaponType.shooting, 3)
+				.build();
+			default -> throw new NullPointerException("Undeclared enemy type");
+		};
+	}
+	
+	
+	public enum EnemyType {
+		Ogre(CreatureRarity.special),
+		SKELETON(CreatureRarity.masterful),
+		ZOMBIE(CreatureRarity.ordinary);
+		
+		private final CreatureRarity creatureRarity;
+		
+		EnemyType(CreatureRarity creatureRarity) {
+			this.creatureRarity = creatureRarity;
+		}
+		public static List<EnemyType> getAll(CreatureRarity rarity){
+			List<EnemyType> enemyTypes = new ArrayList<>();
+			for(EnemyType enemyType : EnemyType.values()){
+				if(enemyType.getCreatureRarity() == rarity){
+					enemyTypes.add(enemyType);
+				}
+			}
+			return enemyTypes;
+		}
+		public CreatureRarity getCreatureRarity() {return creatureRarity;}
+	}
+}
